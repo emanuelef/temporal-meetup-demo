a836fd03a3bc77ad70d83839136e1d8e68d8ae1d
Emanuele Fumagalli <emanuelef@gmail.com>
2024-03-23 22:15:55 +0000

    fix DynamoDB table creation

--- dynamodb-init/init-tables.sh
+++ dynamodb-init/init-tables.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+echo "########### Creating profile ###########"
+aws configure set aws_access_key_id default_access_key --profile=$AWS_DEFAULT_PROFILE
+aws configure set aws_secret_access_key default_secret_key --profile=$AWS_DEFAULT_PROFILE
+aws configure set region us-east-1 --profile=$AWS_DEFAULT_PROFILE
+
+aws configure list
+
 # Wait for DynamoDB to be ready
-until aws --endpoint-url=http://localhost:8000 dynamodb list-tables; do
+until aws $AWS_ENDPOINT dynamodb list-tables; do
   echo "DynamoDB is not ready yet. Waiting..."
   sleep 2
 done
 
 # Create DynamoDB table
-aws --endpoint-url=http://localhost:8000 dynamodb create-table --cli-input-json file://table-definition.json
+aws $AWS_ENDPOINT dynamodb create-table --cli-input-json file:///init-scripts/table-definition.json
 
 # Wait for table to be created
 echo "Waiting for table creation..."
 sleep 5
 
 # Populate DynamoDB table with item
-aws --endpoint-url=http://localhost:8000 dynamodb put-item --table-name ExampleTable --item file://item.json
+aws $AWS_ENDPOINT dynamodb put-item --table-name ExampleTable --item file:///init-scripts/items.json
 
 echo "DynamoDB tables created and populated successfully"
