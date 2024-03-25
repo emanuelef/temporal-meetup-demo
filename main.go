ccd54fdba42c83d591f8204cfb6f39c26cd86de2
Emanuele Fumagalli <emanuelef@gmail.com>
2024-03-25 22:47:31 +0000

    move dynamodb code in a package

--- main.go
+++ main.go
@@ -4,121 +4,17 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"slices"
-	"sync"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/emanuelef/temporal-meetup-demo/otel_instrumentation"
 	"github.com/emanuelef/temporal-meetup-demo/utils"
 	"github.com/gin-gonic/gin"
-	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go-v2/otelaws"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 )
-
-const (
-	DEFAULT_DYNAMO_HOST = "localhost"
-	DEFAULT_DYNAMO_PORT = "8009"
-)
-
-var (
-	once     sync.Once
-	instance *DynamoDBClient
-)
-
-type DynamoDBClient struct {
-	client *dynamodb.Client
-	table  string
-}
-
-func buildLocalConfiguration() (cfg aws.Config, err error) {
-	// Define endpoint resolver for local DynamoDB use
-	host := utils.GetEnv("DYNAMO_HOST", DEFAULT_DYNAMO_HOST)
-	port := utils.GetEnv("DYNAMO_PORT", DEFAULT_DYNAMO_PORT)
-	hostAddress := fmt.Sprintf("http://%s", net.JoinHostPort(host, port))
-
-	resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...any) (aws.Endpoint, error) {
-		return aws.Endpoint{
-			URL: hostAddress, SigningRegion: "localhost",
-		}, nil
-	})
-
-	cfg, err = config.LoadDefaultConfig(context.Background(),
-		config.WithRegion("localhost"),
-		config.WithEndpointResolverWithOptions(resolver),
-		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
-			Value: aws.Credentials{
-				AccessKeyID:     "local",
-				SecretAccessKey: "local",
-			},
-		}),
-	)
-
-	return cfg, err
-}
-
-func NewDynamoDBClient(table string) (*DynamoDBClient, error) {
-	var err error
-	once.Do(func() {
-		cfg, loadErr := buildLocalConfiguration()
-
-		otelaws.AppendMiddlewares(&cfg.APIOptions)
-
-		if loadErr != nil {
-			err = fmt.Errorf("unable to load SDK config: %v", loadErr)
-			return
-		}
-
-		client := dynamodb.NewFromConfig(cfg)
-
-		instance = &DynamoDBClient{
-			client: client,
-			table:  table,
-		}
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return instance, nil
-}
-
-func (c *DynamoDBClient) PutItem(item map[string]types.AttributeValue) error {
-	input := &dynamodb.PutItemInput{
-		TableName: &c.table,
-		Item:      item,
-	}
-
-	_, err := c.client.PutItem(context.TODO(), input)
-	if err != nil {
-		return fmt.Errorf("failed to put item: %v", err)
-	}
-
-	return nil
-}
-
-func (c *DynamoDBClient) GetItem(key map[string]types.AttributeValue) (map[string]types.AttributeValue, error) {
-	input := &dynamodb.GetItemInput{
-		TableName: &c.table,
-		Key:       key,
-	}
-
-	output, err := c.client.GetItem(context.TODO(), input)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get item: %v", err)
-	}
-
-	return output.Item, nil
-}
 
 var tracer trace.Tracer
 
