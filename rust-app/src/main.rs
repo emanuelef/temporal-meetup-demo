fd499b9ea70ffd4caf42c71d9e6cd55f79e00c26
Emanuele Fumagalli <emanuelef@gmail.com>
2024-03-29 14:58:16 +0000

    working on Rust instrumentation

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,9 +1,16 @@
 use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
-use opentelemetry::global::ObjectSafeSpan;
-use opentelemetry::trace::{SpanKind, Status};
-use opentelemetry::sdk::trace::TracerProvider;
-use opentelemetry::{global, sdk::propagation::TraceContextPropagator, trace::Tracer};
+use std::env;
+use std::time::Duration;
+use dotenv::dotenv;
+use opentelemetry::trace::TraceError;
+use opentelemetry::global;
+use opentelemetry_sdk::{propagation::TraceContextPropagator, resource::{
+    OsResourceDetector, ProcessResourceDetector, ResourceDetector,
+    EnvResourceDetector, TelemetryResourceDetector,
+    SdkProvidedResourceDetector,
+}, runtime, trace as sdktrace};
+use opentelemetry_otlp::{self, WithExportConfig};
 
 #[derive(Debug, Serialize)]
 struct GreetResponse {
@@ -25,18 +32,48 @@ async fn greet(name: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().json(serde_json::json!(response))
 }
 
-fn init_tracer() {
+/* fn init_tracer() {
     global::set_text_map_propagator(TraceContextPropagator::new());
     let provider = TracerProvider::builder()
         .with_simple_exporter(SpanExporter::default())
         .build();
     global::set_tracer_provider(provider);
+} */
+
+
+fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let os_resource = OsResourceDetector.detect(Duration::from_secs(0));
+    let process_resource = ProcessResourceDetector.detect(Duration::from_secs(0));
+    let sdk_resource = SdkProvidedResourceDetector.detect(Duration::from_secs(0));
+    let env_resource = EnvResourceDetector::new().detect(Duration::from_secs(0));
+    let telemetry_resource = TelemetryResourceDetector.detect(Duration::from_secs(0));
+    opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(format!(
+                    "{}{}",
+                    env::var("OTEL_EXPORTER_OTLP_ENDPOINT")
+                        .unwrap_or_else(|_| "http://otelcol:4317".to_string()),
+                    "/v1/traces"
+                )), 
+        )
+        .with_trace_config(
+            sdktrace::config()
+                .with_resource(os_resource.merge(&process_resource).merge(&sdk_resource).merge(&env_resource).merge(&telemetry_resource)),
+        )
+        .install_batch(runtime::Tokio)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
+
+    dotenv().ok();
+
     log::warn!("Starting web server on 0.0.0.0:8080");
 
     // Start the Actix web server
